75caaf96888faf73459a24875ef609525a45820c
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-04-06 23:47:06 +0300

    day 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import {moviesData} from "./moviesData";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+console.log(moviesData);
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state ={
+      movies: moviesData
+    }
+  }
+
+  render() {
+    console.log(this);
+    return (
+      <div>
+        {this.state.movies.map(function(movie) {
+          return <p>{movie.title}</p>
+        })}
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return <div>{moviesData[0].title}</div>;
+// }
+
+export default App;
