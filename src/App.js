6eaabae83bbddd55c3d07162ed3e9575147c3677
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-04-08 23:05:53 +0300

    day 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
     super()
 
     this.state = {
-      movies: moviesData
+      movies: moviesData,
+      moviesWillWatch: []
     };
   }
 
@@ -15,25 +16,45 @@ class App extends React.Component {
     const updateMovies = this.state.movies.filter(function(item) {
       return item.id !== movie.id;
     });
-    console.log(updateMovies);
     this.setState({
       movies: updateMovies
     });
   }
 
+  addMovieToWillWatch = movie => {
+    console.log(movie);
+    const updateMoviesToWillWatch = [...this.state.moviesWillWatch, movie];
+
+    this.setState({
+      moviesWillWatch: updateMoviesToWillWatch
+    });
+  }
+
   render() {
-    console.log("render", this.state);
+    console.log("render", this.state, this.temp);
     return (
-      <div>
-        {this.state.movies.map(movie => {
-          return (
-            <MovieItem 
-              key={movie.id} 
-              movie={movie} 
-              removeMovie={this.removeMovie}
-            />
-          );
-        })}
+      <div className="container">
+        <div className="row">
+          <div className="col-9">
+            <div className="row">
+              {this.state.movies.map(movie => {
+                return (
+                  <div className="col-6 mb-4" key={movie.id}>
+                    <MovieItem 
+                      movie={movie} 
+                      removeMovie={this.removeMovie} 
+                      addMovieToWillWatch={this.addMovieToWillWatch} 
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="col-3">
+            <p>Will Watch: {this.state.moviesWillWatch.length}</p>
+          </div>
+        </div>  
+        
       </div>
     );
   }
