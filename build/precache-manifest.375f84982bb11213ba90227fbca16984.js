6d324c1aedb49eba148599133550975b04daca85
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-04-15 15:25:11 +0300

    add gh pages

---
+++ build/precache-manifest.375f84982bb11213ba90227fbca16984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d383ba10b9df529c68961fa877037d97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b19bc60021ca9496a468",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e9f3862a9a532c26bed4",
+    "url": "/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "b19bc60021ca9496a468",
+    "url": "/static/js/2.f237ea5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f237ea5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f3862a9a532c26bed4",
+    "url": "/static/js/main.2c7787ec.chunk.js"
+  },
+  {
+    "revision": "a38c3a50a5632deb5dfa",
+    "url": "/static/js/runtime-main.d55d79b1.js"
+  }
+]);
