5a2af2fb737fb873e27b7dca705e588e9e8abe80
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-04-15 15:27:16 +0300

    Updates

---
+++ precache-manifest.b25c5a1f4be6d5181af34fa80eb50bd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb28e9b822908042a95095275de74105",
+    "url": "/React-Warrior/index.html"
+  },
+  {
+    "revision": "b19bc60021ca9496a468",
+    "url": "/React-Warrior/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e9f3862a9a532c26bed4",
+    "url": "/React-Warrior/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "b19bc60021ca9496a468",
+    "url": "/React-Warrior/static/js/2.f237ea5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Warrior/static/js/2.f237ea5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f3862a9a532c26bed4",
+    "url": "/React-Warrior/static/js/main.2c7787ec.chunk.js"
+  },
+  {
+    "revision": "47979d01790cf817f938",
+    "url": "/React-Warrior/static/js/runtime-main.a345b2f5.js"
+  }
+]);
